c7d1b7a055b40d7582da3e9a922e98ce0e73480d
foivos petros kalfopoulos <zoskiacultus93@yahoo.gr>
2019-06-06 11:36:23 +0300

    Model

---
+++ BlockChain-Project/Models/Block.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockChain_Project.Models
+{
+    public class Block
+    {
+        public decimal Amount { get; set; }
+
+        public string Recipient { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Signature { get; set; }
+
+        public decimal Fees { get; set; }
+
+        public override string ToString()
+        {
+            return Amount.ToString("0.00000000") + Recipient + Sender;
+
+        }
+
+    }
+}
---
+++ BlockChain-Project/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockChain_Project.Models
+{
+    public class Transaction
+    {
+
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        public int Proof { get; set; }
+
+        public string PreviousHash { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Index} [{ TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")}]  Proof : {Proof} PreviousHash : {PreviousHash}";
+        }
+    }
+}
